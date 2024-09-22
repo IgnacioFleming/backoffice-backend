fa557684283c44964dcf6311fe99caf189b67316
Ignacio Fleming <ignacioflemings@gmail.com>
2024-09-22 19:30:00 -0300

    delete product added

--- src/app.js
+++ src/app.js
@@ -6,9 +6,16 @@ const app = express();
 
 app.use(cors());
 
-app.get("/products", (req, res) => {
+app.get("/api/products", (req, res) => {
   const products = mockProducts;
   res.json({ status: "success", payload: products });
+});
+
+app.delete("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const productIndex = mockProducts.findIndex((product) => product.id.toString() === id);
+  const deteteProduct = mockProducts.splice(productIndex, 1);
+  res.json({ status: "success", payload: mockProducts });
 });
 
 const PORT = process.env.PORT || 8080;
