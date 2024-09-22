8bd40ca0c89859b1af4f5b12d7b51c63b54f971c
Ignacio Fleming <ignacioflemings@gmail.com>
2024-09-22 20:10:00 -0300

    finished update products

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import mockProducts from "./assests/mockProducts.js";
 import cors from "cors";
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/products", (req, res) => {
   const products = mockProducts;
+
   res.json({ status: "success", payload: products });
 });
 
@@ -16,6 +17,17 @@ app.delete("/api/products/:id", (req, res) => {
   const productIndex = mockProducts.findIndex((product) => product.id.toString() === id);
   const deteteProduct = mockProducts.splice(productIndex, 1);
   res.json({ status: "success", payload: mockProducts });
+});
+
+app.put("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  const productIndex = mockProducts.findIndex((product) => product.id.toString() === id);
+  const updatedProducts = mockProducts.map((product) => {
+    if (product.id.toString() === id) return { ...product, ...body };
+    return product;
+  });
+  res.json({ status: "success", payload: updatedProducts[productIndex] });
 });
 
 const PORT = process.env.PORT || 8080;
