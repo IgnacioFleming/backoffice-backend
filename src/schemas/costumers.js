a3ee0f9ba5eb6cf52335c94c8146c7d3dea052ca
Ignacio Fleming <ignacioflemings@gmail.com>
2024-09-25 20:30:00 -0300

    add costumer  update logic

---
+++ src/schemas/costumers.js
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const costumerSchema = z.object({
+  id: z.number(),
+  name: z.string().max(50),
+  account_number: z.number().nonnegative(),
+  logo: z.string().url(),
+});
