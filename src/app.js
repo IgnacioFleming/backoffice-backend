9441d553365d42ccf6ece0edf2a72d257482e421
Ignacio Fleming <ignacioflemings@gmail.com>
2024-10-15 19:30:00 -0300

    working with sessions

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ import { costumerRouter } from "./routes/costumers.js";
 import { corsOptions } from "./utils/cors.js";
 import { orderRouter } from "./routes/orders.js";
 import { saleRouter } from "./routes/sales.js";
+import { sessionRouter } from "./routes/sessions.js";
+import initializePassport from "./config/passport.js";
 
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+initializePassport();
 app.use("/api/products", productsRouter);
 app.use("/api/costumers", costumerRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/sales", saleRouter);
+app.use("/api/sessions", sessionRouter);
 
 const PORT = process.env.PORT || 8080;
 
