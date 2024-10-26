961893b3d2c788b2ed3644b3ab582acf58fbebad
Ignacio Fleming <ignacioflemings@gmail.com>
2024-10-26 20:00:00 -0300

    changed session max age

--- src/config/sessionsStorage.js
+++ src/config/sessionsStorage.js
@@ -1,6 +1,6 @@
 import MySQLStore from "express-mysql-session";
 import session from "express-session";
-import { connectionOptions, pool } from "./dbconfig-mysql.js";
+import { connectionOptions } from "./dbconfig-mysql.js";
 import mysql from "mysql2";
 
 const mySQLStore = MySQLStore(session);
