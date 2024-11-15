47c40a504a1feb27dcc202fd5659fd9463e7c611
Ignacio Fleming <ignacioflemings@gmail.com>
2024-11-15 20:14:00 -0300

    .

--- src/utils/errors/CustomError.js
+++ src/utils/errors/CustomError.js
@@ -5,6 +5,5 @@ export default class CustomError extends Error {
     super(message);
     this.name = name || ERRORS.UNHANDLED.name;
     this.statusCode = statusCode || 500;
-    // Error.captureStackTrace(this, this.constructor);
   }
 }
