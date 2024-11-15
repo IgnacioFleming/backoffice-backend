81913e1fa62b87317e77b87830819c63fe4f931e
Ignacio Fleming <ignacioflemings@gmail.com>
2024-11-15 20:30:00 -0300

    logs

--- src/middlewares/auth/auth.js
+++ src/middlewares/auth/auth.js
@@ -2,6 +2,8 @@ import { createCustomError } from "../../utils/errors/errorFactory.js";
 import { ERRORS } from "../../utils/errors/errorTypes.js";
 
 export const auth = async (req, res, next) => {
+  console.log(req.user, "req.user");
+  console.log(req.session, "req.session");
   if (!req.isAuthenticated()) {
     const error = createCustomError(ERRORS.AUTH);
     next(error);
