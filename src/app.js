2030db39831ff0fd41b99f587a86aaf788cbe707
Ignacio Fleming <ignacioflemings@gmail.com>
2024-11-15 20:42:00 -0300

    .

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "https://management-system-frontend-five.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
