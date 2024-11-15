e20f6c34d870de24f17a3e146891ac297cab9269
Ignacio Fleming <ignacioflemings@gmail.com>
2024-11-15 20:20:00 -0300

    .

--- src/middlewares/auth/passportCall.js
+++ src/middlewares/auth/passportCall.js
@@ -13,9 +13,14 @@ export const passportCall = (strategy) => {
         const error = createCustomError(ERRORS.AUTH, info?.message);
         return next(error);
       }
+      console.log(user);
+      console.log(req.user);
+      console.log(req.session);
+
       req.user = user;
       req.session.passport = { user: { id: user.id } };
       req.isAuthenticated = () => req.session.passport.user && true;
+      console.log(req.isAuthenticated());
       next();
     })(req, res, next);
   };
